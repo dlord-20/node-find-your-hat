9c756fb004b7bb8f0624084d0cf8e25893eff3d8
dlord-20 <dthomaslord@gmail.com>
2022-09-14 20:25:13 +0300

    Added getRandomCharacter and generateField functins -> Not tested yet

--- main.js
+++ main.js
@@ -6,5 +6,27 @@ const fieldCharacter = '░';
 const pathCharacter = '*';
 
 class Field {
+
   
+}
+
+const getRandomCharacter = () => {
+    const randNum = Math.floor(Math.random()* 2);
+    console.log(randNum);
+    const options = ['░', 'O'];
+    const character = options.randNum;
+    return character;
+}
+
+const generateField = (width, height) => {
+    const fieldArray = [];
+    for(let i = 0; i < height; i++) {
+        const newRow = [];
+        for(let j = 0; j < width; j++) {
+            newRow.push(getRandomCharacter());
+        }
+        fieldArray.push(newRow);
+    }
+
+    return fieldArray;
 }
